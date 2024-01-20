9f5c6475a58150c463c548bf393524ddcf386325
AlexGame2012 <139368285+AlexGame2012@users.noreply.github.com>
2024-01-20 22:11:44 +0700

    Update client.js

--- client.js
+++ client.js
@@ -762,9 +762,6 @@ player.ContextedProperties.SkinType.Value = 0; 
 player.ContextedProperties.MaxHp.Value = 100;  
 
 player.Ui.Hint.Value = "РЕЖИМ ПВП ВЫКЛ!";   
-
-}
-
 });
 
 var ин = AreaPlayerTriggerService.Get("ин");
